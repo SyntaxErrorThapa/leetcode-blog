cafa562558342033e7c533b0519f8fbc76b4ec12
PratikThapa24 <prajwaljungthapa@gmail.com>
2024-07-29 18:45:21 -0400

    Working on auth

--- leetcode/server/routes/GoogleAuth.js
+++ leetcode/server/routes/GoogleAuth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+
 const router = Router();
 
 router.post("/auth-google", async (req, res) => {
--- leetcode/server/server.js
+++ leetcode/server/server.js
@@ -1,7 +1,8 @@
 // server.js or index.js
 import express from "express";
 import questionRouter from "./routes/Question.js";
-import googleAuthRouter from "./routes/GoogleAuth.js";  
+import googleAuthRouter from "./routes/GoogleAuth.js";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 const app = express();
 const port = 5000;
@@ -9,9 +10,28 @@ const port = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configure the Google strategy
+passport.use(new GoogleStrategy({
+  clientID: process.env.AUTHCLIENTID, 
+  clientSecret: process.env.AUTHCLIENTSECRET,
+  callbackURL: process.env.AUTHCALLBACKURL, 
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  return done(null, profile);
+}))
+
+// Serialize and deserialize user instances to and from the session
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 // Use the question router
-app.use('/', questionRouter);
-app.use('/', googleAuthRouter);
+app.use("/", questionRouter);
+app.use("/", googleAuthRouter);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
